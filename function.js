aa06694fcb216dee40fa228e77594cb9b64aebcd
asdfghjklesterio <delcarmenlester@gmail.com>
2021-01-07 20:21:22 +0800

    added jsfunction

--- function.js
+++ function.js
@@ -5,3 +5,23 @@ function openNav(){
 function closeNav(){
     document.getElementById("navigation").style.width = "0";
 }
+
+function rdsubjectp()
+{
+    window.location.href = "subjectpage.html";
+}
+
+function rdlistwp()
+{
+    window.location.href = "listworks.html";
+}
+
+function rdfolderp()
+{
+    window.location.href = "folderpage.html";
+}
+
+function rdactp()
+{
+    window.location.href = "activitiypage.html";
+}
